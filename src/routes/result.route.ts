3e59d97f67ee01adee226d6a8f4dbe61bf135d97
XERF99 <francisco.rodriguez@epn.edu.ec>
2024-11-06 14:25:57 -0500

    feat: update Result services

--- src/routes/result.route.ts
+++ src/routes/result.route.ts
@@ -6,14 +6,15 @@ import {
 } from "../controllers/result.controller";
 import {schemaVerifierMiddleware} from "../middlewares/schemaVerifier.middleware";
 import {createResultSchema, resultSchemaParams, updateResultSchema} from "../schemasJoi/result.schema";
+import {paginationSchema} from "../schemasJoi/pagination.schema";
 
 const router = Router();
 
 router.post('/',[schemaVerifierMiddleware({body: createResultSchema})], createResultController);
 router.put('/:id', [schemaVerifierMiddleware({params: resultSchemaParams}), schemaVerifierMiddleware({body: updateResultSchema})], updateResultController);
 router.get('/:id', [schemaVerifierMiddleware({params: resultSchemaParams})], getResultByIdController)
-router.get('/:id', [schemaVerifierMiddleware({params: resultSchemaParams})], deleteResultController);
-router.get('/', [schemaVerifierMiddleware({query: resultSchemaParams})], listResultController);
+router.delete('/:id', [schemaVerifierMiddleware({params: resultSchemaParams})], deleteResultController);
+router.get('/', [schemaVerifierMiddleware({query: paginationSchema})], listResultController);
 
 export { router };
 
